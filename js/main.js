48c9b2485fd3aee0e4bccdc42460f1bdaba3521c
zholkin85 <zholkin85@yandex.ru>
2021-03-10 01:32:00 +0300

    create: added yandex cart

--- js/main.js
+++ js/main.js
@@ -15,3 +15,15 @@ const swiper = new Swiper('.swiper-container', {
         enabled: true,
     },
 })
+
+ymaps.ready(init)
+
+function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map('map', {
+        center: [7.838358, 98.298838],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 15,
+    })
+}
