faec7564d3fedeb96a2a8bdd7b10082c113c4a3f
zholkin85 <zholkin85@yandex.ru>
2021-03-10 22:53:49 +0300

    fix & create: fixed index.html & added 'reviews'

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
 
     // Navigation arrows
     navigation: {
-        nextEl: '.slider-button--next',
-        prevEl: '.slider-button--prev',
+        nextEl: '.hotel-slider__button--next',
+        prevEl: '.hotel-slider__button--prev',
     },
 
     effect: 'fade',
+
+    //keyboard control
+    keyboard: {
+        enabled: true,
+    },
+})
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.hotel-slider__button--next',
+        prevEl: '.hotel-slider__button--prev',
+    },
 
     //keyboard control
     keyboard: {
