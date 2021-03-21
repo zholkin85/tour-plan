afb734e0ad73d2a0f86a5764ad4c8db48380bced
zholkin85 <zholkin85@yandex.ru>
2021-03-21 23:24:02 +0300

    create: added js function for block 'modal'

--- js/main.js
+++ js/main.js
@@ -1,57 +1,90 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-    // Optional parameters
-    loop: true,
+$(document).ready(function() {
+    const hotelSlider = new Swiper('.hotel-slider', {
+        // Optional parameters
+        loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.hotel-slider__button--next',
-        prevEl: '.hotel-slider__button--prev',
-    },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.hotel-slider__button--next',
+            prevEl: '.hotel-slider__button--prev',
+        },
 
-    effect: 'fade',
+        effect: 'fade',
 
-    //keyboard control
-    keyboard: {
-        enabled: true,
-    },
-})
-
-const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.reviews-slider__button--next',
-        prevEl: '.reviews-slider__button--prev',
-    },
-
-    //keyboard control
-    keyboard: {
-        enabled: true,
-    },
-
-    //autoplay
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-})
-
-ymaps.ready(init)
-
-function init() {
-    // Создание карты.
-    var myMap = new ymaps.Map('map', {
-        center: [7.838358, 98.298838],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 15,
+        //keyboard control
+        keyboard: {
+            enabled: true,
+        },
     })
-}
 
-var menuButton = document.querySelector('.menu-button')
-menuButton.addEventListener('click', function() {
-    console.log('Клик по кнопке меню')
-    document.querySelector('.navbar__nav').classList.toggle('navbar__nav--visible')
+    const reviewsSlider = new Swiper('.reviews-slider', {
+        // Optional parameters
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.reviews-slider__button--next',
+            prevEl: '.reviews-slider__button--prev',
+        },
+
+        //keyboard control
+        keyboard: {
+            enabled: true,
+        },
+
+        //autoplay
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+    })
+
+    ymaps.ready(init)
+
+    function init() {
+        // Создание карты.
+        var myMap = new ymaps.Map('map', {
+            center: [7.838358, 98.298838],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 15,
+        })
+    }
+
+    var menuButton = $('.menu-button')
+    menuButton.on('click', function() {
+        $('.navbar__nav').toggleClass('navbar__nav--visible')
+    })
+
+    var modalButton = $('[data-toggle=modal]')
+    var closeModalButton = $('.modal__close')
+    var closeEsc = $('[data-toggle=modal]')
+    modalButton.on('click', openModal)
+    closeModalButton.on('click', closeModal)
+    closeEsc.on('keydown', escClose)
+
+    function openModal() {
+        var targetModal = $(this).attr('data-href')
+        $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible')
+        $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible')
+    }
+
+    function closeModal(event) {
+        event.preventDefault()
+        var modalOverlay = $('.modal__overlay')
+        var modalDialog = $('.modal__dialog')
+        modalOverlay.removeClass('modal__overlay--visible')
+        modalDialog.removeClass('modal__dialog--visible')
+    }
+
+    function escClose() {
+        modalButton.keydown(function(event) {
+            if (event.which == 27) {
+                var modalOverlay = $('.modal__overlay')
+                var modalDialog = $('.modal__dialog')
+                modalOverlay.removeClass('modal__overlay--visible')
+                modalDialog.removeClass('modal__dialog--visible')
+            }
+        })
+    }
 })
