ea2782612888df6e16ebd1a4f50861b4eeb4728f
zholkin85 <zholkin85@yandex.ru>
2021-03-28 22:32:10 +0300

    fix: adaptive & forms

--- js/main.js
+++ js/main.js
@@ -115,10 +115,12 @@ $(document).ready(function() {
                     phone: {
                         required: true,
                         phone: true,
+                        minlength: '3',
                     },
                     email: {
                         required: true,
                         email: true,
+                        minlength: '2',
                     },
                 },
                 messages: {
@@ -138,7 +140,39 @@ $(document).ready(function() {
         })
         //маска номера телефона
     $('.phone').each(function() {
-        $(this).mask('+7 (999) 999-99-99', { placeholder: '+7 (999) 999-99-99' })
+        $(this).mask('+7 (999) 999-99-99', { placeholder: '+7 (999) 999-99-99' }, { minlength: '11' })
     })
     AOS.init()
+
+    // Отправка данных на сервер
+    function send(event, php) {
+        console.log('Отправка запроса')
+        event.preventDefault ? event.preventDefault() : (event.returnValue = false)
+        var req = new XMLHttpRequest()
+        req.open('POST', php, true)
+        req.onload = function() {
+            if (req.status >= 200 && req.status < 400) {
+                json = JSON.parse(this.response) // Ебанный internet explorer 11
+                console.log(json)
+
+                // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+                if (json.result == 'success') {
+                    // Если сообщение отправлено
+                    alert('Сообщение отправлено')
+                } else {
+                    // Если произошла ошибка
+                    alert('Ошибка. Сообщение не отправлено')
+                }
+                // Если не удалось связаться с php файлом
+            } else {
+                alert('Ошибка сервера. Номер: ' + req.status)
+            }
+        }
+
+        // Если не удалось отправить запрос. Стоит блок на хостинге
+        req.onerror = function() {
+            alert('Ошибка отправки запроса')
+        }
+        req.send(new FormData(event.target))
+    }
 })
